1374466c7621dd493875e8c55b2773f20ec81c8c
Taha <taha.karim@agcocorp.com>
2020-07-28 13:08:32 -0500

    updated_large_field

--- helpers.py
+++ helpers.py
@@ -6,52 +6,52 @@ def helper_random_fieldpoint(corner1, corner2):
     Function to return random point in field
 
     :param corner1: the bottom left corner of the field
-    :type corner1: tuple
+    :type corner1: dict
 
     :param corner2: the top right corner of the field
-    :type corner2: tuple
+    :type corner2: dict
 
     :return: returns a lat/long in the field
-    :rtype: tuple
+    :rtype: dict
     """
 
-    x = random.uniform(corner1[0], corner2[0])
-    y = random.uniform(corner1[1], corner2[1])
-    point = (x, y)
+    x = random.uniform(corner1['lat'], corner2['lat'])
+    y = random.uniform(corner1['lng'], corner2['lng'])
+    point = {'lat': x, 'lng': y}
 
     return point
 
 
 def helper_random_gate(corner1, corner2):
     """
-    Function to return random point in field
+    Function to return random gate in field
 
     :param corner1: the bottom left corner of the field
-    :type corner1: tuple
+    :type corner1: dict
 
     :param corner2: the top right corner of the field
-    :type corner2: tuple
+    :type corner2: dict
 
     :return: returns a lat/long in the field
-    :rtype: tuple
+    :rtype: dict
     """
 
     side = random.randint(1, 4)
 
     if side == 1:
-        gatex = random.uniform(corner1[0], corner2[0])
-        gatey = corner2[1]
+        gatex = random.uniform(corner1['lat'], corner2['lat'])
+        gatey = corner2['lng']
     elif side == 2:
-        gatey = random.uniform(corner1[1], corner2[1])
-        gatex = corner2[0]
+        gatey = random.uniform(corner1['lng'], corner2['lng'])
+        gatex = corner2['lat']
     elif side == 3:
-        gatex = random.uniform(corner1[0], corner2[0])
-        gatey = corner1[1]
+        gatex = random.uniform(corner1['lat'], corner2['lat'])
+        gatey = corner1['lng']
     elif side == 4:
-        gatey = random.uniform(corner1[1], corner2[1])
-        gatex = corner1[0]
+        gatey = random.uniform(corner1['lng'], corner2['lng'])
+        gatex = corner1['lat']
 
-    point = (gatex, gatey)
+    point = {'lat': gatex, 'lng': gatey}
 
     return point
 
