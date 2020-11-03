f80e7b7f05ef70b542f135a25c7031f16eb7cf1f
Taha <taha.karim@agcocorp.com>
2020-11-03 10:58:11 -0600

    post_testcases_change

--- conftest.py
+++ conftest.py
@@ -57,37 +57,37 @@ def pytest_configure(config):
     auth = config.getoption('auth')
     level = config.getoption('level')
     params.global_api = config.getoption('api')
-    # if api != 'v1beta2':
-    #     payload = deepcopy(params.payload_optional)
-    #     payload['field']['boundary']['boundary'] = params.quarter_circle_field
-    #     payload['field']['gates'][0]['point'] = random.choice(params.quarter_circle_field)
-    #     payload['row_direction'][0] = helpers.helper_random_fieldpoint({'lat': 37.792516, 'lng': -97.399534},
-    #                                                                    {'lat': 37.794469, 'lng': -97.403632})
-    #     payload['row_direction'][1] = helpers.helper_random_fieldpoint({'lat': 37.792516, 'lng': -97.403632},
-    #                                                                    {'lat': 37.794469, 'lng': -97.399534})
-    #     payload['field']['soil_type'] = helpers.helper_random_soiltype()
-    #
-    # else:
-    #     payload = deepcopy(params.payload)
-    #     payload['field']['boundary']['boundary'] = params.quarter_circle_field
-    #
-    # payload = json.dumps(payload)
-    #
-    # response = plans_post_payload(env, api, auth, level, payload)
-    # json_response = response.json()
-    # plan_id = json_response['plan_id']
-    # print("Plan_ID: {0}".format(plan_id))
-    #
-    # response = plans_get_by_id(env, api, auth, level, plan_id)
-    # json_response = response.json()
-    #
-    # while json_response['status']['is_complete'] is False:
-    #     sleep(5)
-    #     response = plans_get_by_id(env, api, auth, level, plan_id)
-    #     json_response = response.json()
-    #
-    # if json_response['status']['has_error'] is False:
-    #     params.test_plan_id = json_response['plan_id']
-    # else:
-    #     print(json_response)
-    #     exit("Setup Failed")
+    if api != 'v1beta2':
+        payload = deepcopy(params.payload_optional)
+        payload['field']['boundary']['boundary'] = params.quarter_circle_field
+        payload['field']['gates'][0]['point'] = random.choice(params.quarter_circle_field)
+        payload['row_direction'][0] = helpers.helper_random_fieldpoint({'lat': 37.792516, 'lng': -97.399534},
+                                                                       {'lat': 37.794469, 'lng': -97.403632})
+        payload['row_direction'][1] = helpers.helper_random_fieldpoint({'lat': 37.792516, 'lng': -97.403632},
+                                                                       {'lat': 37.794469, 'lng': -97.399534})
+        payload['field']['soil_type'] = helpers.helper_random_soiltype()
+
+    else:
+        payload = deepcopy(params.payload)
+        payload['field']['boundary']['boundary'] = params.quarter_circle_field
+
+    payload = json.dumps(payload)
+
+    response = plans_post_payload(env, api, auth, level, payload)
+    json_response = response.json()
+    plan_id = json_response['plan_id']
+    print("Plan_ID: {0}".format(plan_id))
+
+    response = plans_get_by_id(env, api, auth, level, plan_id)
+    json_response = response.json()
+
+    while json_response['status']['is_complete'] is False:
+        sleep(5)
+        response = plans_get_by_id(env, api, auth, level, plan_id)
+        json_response = response.json()
+
+    if json_response['status']['has_error'] is False:
+        params.test_plan_id = json_response['plan_id']
+    else:
+        print(json_response)
+        exit("Setup Failed")
